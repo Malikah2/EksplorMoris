a4619013751569d403ceda8b555a0abc83a51c1c
Malikah2 <bibi.nauthoo4@umail.uom.ac.mu>
2023-08-08 01:23:37 +0400

    Mon ajoute forgot password avec ene transition kan sanz interface

--- lib/pages/welcome_page.dart.dart
+++ lib/pages/welcome_page.dart.dart
@@ -30,7 +30,7 @@ class WelcomePage extends StatelessWidget {
                 ),
                 const SizedBox(height: 20),
                 const Text(
-                  "All your vacations destinations are here,\nenjoy your holiday",
+                  "Explore your vacations destinations are here,\nenjoy your holiday",
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     color: Colors.black54,
